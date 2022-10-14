0603b101df001a9ee2cf7babb806ab60e7ac77d7
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2022-10-13 23:31:48 -0400

    + fixes

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -17,181 +17,215 @@
             }
         }
         currentSymbol = '.';
-        bool activeTetromino = true;
         NewTetromino();
         while (true)
         {
-            if (!activeTetromino)
-            {
-                if (board[0, position] != '.')
-                {
-                    break;
-                }
-                NewTetromino();
-                activeTetromino = true;
-                await Task.Delay(1000);
-            }
-            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
-            cancellationTokenSource.CancelAfter(1000);
-            ConsoleKey input = (await ReadKeyAsync(cancellationTokenSource.Token)).Key;
-            switch (input)
-            {
-                case ConsoleKey.LeftArrow:
-                case ConsoleKey.A:
-                    int i = 0;
-                    for (; i < 4; i++)
-                    {
-                        if (currentTetromino[i].Item1 == 0 || board[currentTetromino[i].Item1 - 1, currentTetromino[i].Item2] != '.')
-                        {
-                            break;
-                        }
-                    }
-                    if (i != 4)
-                    {
-                        break;
-                    }
-                    position--;
-                    for (i = 0; i < 4; i++)
-                    {
-                        board[currentTetromino[i].Item1, currentTetromino[i].Item2] = '.';
-                    }
-                    for (i = 0; i < 4; i++)
-                    {
-                        currentTetromino[i].Item1--;
-                        board[currentTetromino[i].Item1, currentTetromino[i].Item2] = currentSymbol;
-                    }
-                    break;
-                case ConsoleKey.RightArrow:
-                case ConsoleKey.D:
-                    int j = 0;
-                    for (; j < 4; j++)
-                    {
-                        if (currentTetromino[j].Item1 == 9 || board[currentTetromino[j].Item1 + 1, currentTetromino[j].Item2] != '.')
-                        {
-                            break;
-                        }
-                    }
-                    if (j != 4)
-                    {
-                        break;
-                    }
-                    position++;
-                    for (j = 0; j < 4; j++)
-                    {
-                        board[currentTetromino[j].Item1, currentTetromino[j].Item2] = '.';
-                    }
-                    for (j = 0; j < 4; j++)
-                    {
-                        currentTetromino[j].Item1++;
-                        board[currentTetromino[j].Item1, currentTetromino[j].Item2] = currentSymbol;
-                    }
-                    break;
-                case ConsoleKey.UpArrow: //rotation 90 degrees
-                    switch (currentSymbol)
-                    {
-                        case '#':
-                            if (currentRotation == 0)
-                            {
-                                if (board[currentTetromino[0].Item1 - 1, currentTetromino[0].Item2 + 1] != '.')
-                                {
-                                    break;
-                                }
-                                if (board[currentTetromino[2].Item1 + 1, currentTetromino[2].Item2 - 1] != '.')
-                                {
-                                    break;
-                                }
-                                if (board[currentTetromino[3].Item1 + 2, currentTetromino[3].Item2 - 2] != '.')
-                                {
-                                    break;
-                                }
-                                for (j = 0; j < 4; j++)
-                                {
-                                    board[currentTetromino[j].Item1, currentTetromino[j].Item2] = '.';
-                                }
-                                currentTetromino[0].Item1--;
-                                currentTetromino[0].Item2++;
-                                currentTetromino[2].Item1++;
-                                currentTetromino[2].Item2--;
-                                currentTetromino[3].Item1 += 2;
-                                currentTetromino[3].Item2 -= 2;
-                                currentRotation = 1;
-                            }
-                            else
-                            {
-                                if (board[currentTetromino[0].Item1 + 1, currentTetromino[0].Item2 - 1] != '.')
-                                {
-                                    break;
-                                }
-                                if (board[currentTetromino[2].Item1 - 1, currentTetromino[2].Item2 + 1] != '.')
-                                {
-                                    break;
-                                }
-                                if (board[currentTetromino[3].Item1 - 2, currentTetromino[3].Item2 + 2] != '.')
-                                {
-                                    break;
-                                }
-                                for (j = 0; j < 4; j++)
-                                {
-                                    board[currentTetromino[j].Item1, currentTetromino[j].Item2] = '.';
-                                }
-                                currentTetromino[0].Item1++;
-                                currentTetromino[0].Item2--;
-                                currentTetromino[2].Item1--;
-                                currentTetromino[2].Item2++;
-                                currentTetromino[3].Item1 -= 2;
-                                currentTetromino[3].Item2 += 2;
-                                currentRotation = 0;
-                            }
-                            break;
-                        case '&':
-                            break;
-                        case '@':
-                            break;
-                        case '?':
-                            break;
-                        case '$':
-                            break;
-                        case '=':
-                            break;
-                        case 'x':
-                            break;
-                    }
-                    break;
-            }
             for (int i = 0; i < 4; i++)
             {
                 if (currentTetromino[i].Item2 == 19 || (board[currentTetromino[i].Item1, currentTetromino[i].Item2 + 1] != '.' && !currentTetromino.Contains((currentTetromino[i].Item1, currentTetromino[i].Item2 + 1))))
                 {
-                    Console.WriteLine("false!!!");
-                    await Task.Delay(1000);
-                    activeTetromino = false;
-                    break;
+                    if (board[position, 0] != '.')
+                    {
+                        goto GameOver;
+                    }
+                    NewTetromino();
                 }
             }
-            if (activeTetromino)
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+            cancellationTokenSource.CancelAfter(1000);
+            while (!cancellationTokenSource.Token.IsCancellationRequested)
             {
-                for (int i = 0; i < 4; i++)
+                ConsoleKey input = (await ReadKeyAsync(cancellationTokenSource.Token)).Key;
+                switch (input)
                 {
-                    board[currentTetromino[i].Item1, currentTetromino[i].Item2] = '.';
-                }
-                for (int i = 0; i < 4; i++)
-                {
-                    currentTetromino[i].Item2++;
-                    board[currentTetromino[i].Item1, currentTetromino[i].Item2] = currentSymbol;
+                    case ConsoleKey.LeftArrow:
+                    case ConsoleKey.A:
+                        int i = 0;
+                        for (; i < 4; i++)
+                        {
+                            if (currentTetromino[i].Item1 == 0 || (board[currentTetromino[i].Item1 - 1, currentTetromino[i].Item2] != '.' && !currentTetromino.Contains((currentTetromino[i].Item1 - 1, currentTetromino[i].Item2))))
+                            {
+                                break;
+                            }
+                        }
+                        if (i != 4)
+                        {
+                            break;
+                        }
+                        position--;
+                        for (i = 0; i < 4; i++)
+                        {
+                            board[currentTetromino[i].Item1, currentTetromino[i].Item2] = '.';
+                        }
+                        for (i = 0; i < 4; i++)
+                        {
+                            currentTetromino[i].Item1--;
+                            board[currentTetromino[i].Item1, currentTetromino[i].Item2] = currentSymbol;
+                        }
+                        Render();
+                        break;
+                    case ConsoleKey.RightArrow:
+                    case ConsoleKey.D:
+                        int j = 0;
+                        for (; j < 4; j++)
+                        {
+                            if (currentTetromino[j].Item1 == 9 || (board[currentTetromino[j].Item1 + 1, currentTetromino[j].Item2] != '.' && !currentTetromino.Contains((currentTetromino[j].Item1 + 1, currentTetromino[j].Item2))))
+                            {
+                                break;
+                            }
+                        }
+                        if (j != 4)
+                        {
+                            break;
+                        }
+                        position++;
+                        for (j = 0; j < 4; j++)
+                        {
+                            board[currentTetromino[j].Item1, currentTetromino[j].Item2] = '.';
+                        }
+                        for (j = 0; j < 4; j++)
+                        {
+                            currentTetromino[j].Item1++;
+                            board[currentTetromino[j].Item1, currentTetromino[j].Item2] = currentSymbol;
+                        }
+                        Render();
+                        break;
+                    case ConsoleKey.Spacebar:
+                        while (true)
+                        {
+                            int d = 0;
+                            for (; d < 4; d++)
+                            {
+                                if (currentTetromino[d].Item2 == 19 || (board[currentTetromino[d].Item1, currentTetromino[d].Item2 + 1] != '.' && !currentTetromino.Contains((currentTetromino[d].Item1, currentTetromino[d].Item2 + 1))))
+                                {
+                                    break;
+                                }
+                            }
+                            if (d != 4)
+                            {
+                                break;
+                            }
+                            for (d = 0; d < 4; d++)
+                            {
+                                board[currentTetromino[d].Item1, currentTetromino[d].Item2] = '.';
+                            }
+                            for (d = 0; d < 4; d++)
+                            {
+                                currentTetromino[d].Item2++;
+                                board[currentTetromino[d].Item1, currentTetromino[d].Item2] = currentSymbol;
+                            }
+                        }
+                        goto CallRender;
+                    case ConsoleKey.UpArrow:
+                        switch (currentSymbol)
+                        {
+                            case '#':
+                                if (currentRotation == 0)
+                                {
+                                    if (board[currentTetromino[0].Item1 - 1, currentTetromino[0].Item2 + 1] != '.')
+                                    {
+                                        break;
+                                    }
+                                    if (board[currentTetromino[2].Item1 + 1, currentTetromino[2].Item2 - 1] != '.')
+                                    {
+                                        break;
+                                    }
+                                    if (board[currentTetromino[3].Item1 + 2, currentTetromino[3].Item2 - 2] != '.')
+                                    {
+                                        break;
+                                    }
+                                    for (j = 0; j < 4; j++)
+                                    {
+                                        board[currentTetromino[j].Item1, currentTetromino[j].Item2] = '.';
+                                    }
+                                    currentTetromino[0].Item1--;
+                                    currentTetromino[0].Item2++;
+                                    currentTetromino[2].Item1++;
+                                    currentTetromino[2].Item2--;
+                                    currentTetromino[3].Item1 += 2;
+                                    currentTetromino[3].Item2 -= 2;
+                                    currentRotation = 1;
+                                    Render();
+                                }
+                                else
+                                {
+                                    if (board[currentTetromino[0].Item1 + 1, currentTetromino[0].Item2 - 1] != '.')
+                                    {
+                                        break;
+                                    }
+                                    if (board[currentTetromino[2].Item1 - 1, currentTetromino[2].Item2 + 1] != '.')
+                                    {
+                                        break;
+                                    }
+                                    if (board[currentTetromino[3].Item1 - 2, currentTetromino[3].Item2 + 2] != '.')
+                                    {
+                                        break;
+                                    }
+                                    for (j = 0; j < 4; j++)
+                                    {
+                                        board[currentTetromino[j].Item1, currentTetromino[j].Item2] = '.';
+                                    }
+                                    currentTetromino[0].Item1++;
+                                    currentTetromino[0].Item2--;
+                                    currentTetromino[2].Item1--;
+                                    currentTetromino[2].Item2++;
+                                    currentTetromino[3].Item1 -= 2;
+                                    currentTetromino[3].Item2 += 2;
+                                    currentRotation = 0;
+                                    Render();
+                                }
+                                break;
+                            case '&':
+                                break;
+                            case '@':
+                                break;
+                            case '?':
+                                break;
+                            case '$':
+                                break;
+                            case '=':
+                                break;
+                            case 'x':
+                                break;
+                        }
+                        break;
                 }
             }
-            Console.Clear();
-            for (int i = 0; i < 20; i++)
+            for (int i = 0; i < 4; i++)
             {
-                Console.WriteLine($" {board[0, i]} {board[1, i]} {board[2, i]} {board[3, i]} {board[4, i]} {board[5, i]} {board[6, i]} {board[7, i]} {board[8, i]} {board[9, i]}");
+                board[currentTetromino[i].Item1, currentTetromino[i].Item2] = '.';
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                currentTetromino[i].Item2++;
+                board[currentTetromino[i].Item1, currentTetromino[i].Item2] = currentSymbol;
+            }
+            CallRender:
+            {
+                Render();
             }
         }
-        Console.WriteLine($"You've completed {lines} {(lines == 1 ? "line" : "lines")}!\nPlay again? (y or n)");
-        char c = Console.ReadKey(true).KeyChar;
-        Console.Clear();
-        if (c == 'y' || c == 'Y')
+        GameOver:
         {
-            await Main();
+            Console.WriteLine($"You've completed {lines} {(lines == 1 ? "line" : "lines")}!\nPlay again? (y or n)");
+            char c = Console.ReadKey(true).KeyChar;
+            Console.Clear();
+            if (c == 'y' || c == 'Y')
+            {
+                await Main();
+            }
+        }
+    }
+    static void Render()
+    {
+        Console.Clear();
+        for (int i = 0; i < 4; i++)
+        {
+            board[currentTetromino[i].Item1, currentTetromino[i].Item2] = currentSymbol;
+        }
+        for (int i = 0; i < 20; i++)
+        {
+            Console.WriteLine($" {board[0, i]} {board[1, i]} {board[2, i]} {board[3, i]} {board[4, i]} {board[5, i]} {board[6, i]} {board[7, i]} {board[8, i]} {board[9, i]}");
         }
     }
     static bool NewTetromino()
@@ -210,10 +244,12 @@
                 currentTetromino[1] = (position, 1);
                 currentTetromino[2] = (position, 2);
                 currentTetromino[3] = (position, 3);
+                Console.WriteLine(position);
                 board[position, 0] = '#';
                 board[position, 1] = '#';
                 board[position, 2] = '#';
                 board[position, 3] = '#';
+                currentRotation = 0;
                 return true;
             /*case 1:
                 currentSymbol = '&';
