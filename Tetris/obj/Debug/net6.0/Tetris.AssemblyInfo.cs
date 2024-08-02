07ae0d31bc4c5dd9e9e76218863479fe2c8e0eb3
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2024-08-01 23:39:30 -0400

    misc

--- Tetris/obj/Debug/net6.0/Tetris.AssemblyInfo.cs
+++ Tetris/obj/Debug/net6.0/Tetris.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tetris")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+535a86b8e0a33866169515db53d71e064e823848")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tetris")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tetris")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
